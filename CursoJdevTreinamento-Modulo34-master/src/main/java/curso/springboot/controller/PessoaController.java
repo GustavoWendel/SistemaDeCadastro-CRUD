59e6facdca86292502bee70f7689e678a99930ad
gustavo.borges <gustavo.borges@basis.com.br>
2020-11-26 11:40:44 -0300

    Retirado métodos referente ao objeto Telefone

--- CursoJdevTreinamento-Modulo34-master/src/main/java/curso/springboot/controller/PessoaController.java
+++ CursoJdevTreinamento-Modulo34-master/src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,7 +1,6 @@
 package curso.springboot.controller;
 
-import curso.springboot.model.Telefone;
-import curso.springboot.repository.TelefoneRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -26,9 +25,6 @@ public class PessoaController {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
-	@Autowired
-	private TelefoneRepository telefoneRepository;
-
 	@GetMapping(value = "/cadastroPessoa")
 	public ModelAndView inicio() {
 
@@ -49,7 +45,7 @@ public class PessoaController {
 			modelAndView.addObject("pessoaObj", pessoa);
 
 			List<String> msg = new ArrayList<String>();
-			for(ObjectError objectError : bindingResult.getAllErrors()){
+				for(ObjectError objectError : bindingResult.getAllErrors()){
 				msg.add(objectError.getDefaultMessage()); //Vem das anotações @NotEmpty e outras
 			}
 			
@@ -107,42 +103,6 @@ public class PessoaController {
 		return modelAndView;
 	}
 
-	@GetMapping("/telefones/{idPessoa}")
-	public ModelAndView telefones(@PathVariable("idPessoa") Long idPessoa) {
-		Optional<Pessoa> pessoa = pessoaRepository.findById(idPessoa);
 
-		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
-		modelAndView.addObject("pessoaObj", pessoa.get());
-		modelAndView.addObject("telefones", telefoneRepository.getTelefones(idPessoa));
-
-		return modelAndView;
-	}
-
-	@PostMapping("**/addFonePessoa/{pessoaid}")
-	public ModelAndView addFonePessoa(Telefone telefone,
-									  @PathVariable("pessoaid") Long pessoaid){
-
-		Pessoa pessoa = pessoaRepository.findById(pessoaid).get();
-		telefone.setPessoa(pessoa);
-
-		telefoneRepository.save(telefone);
-		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
-		modelAndView.addObject("pessoaObj", pessoa);
-		modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoaid));
-		return modelAndView;
-	}
-
-	@GetMapping("/removerTelefone/{idTelefone}")
-	public ModelAndView removerTelefone(@PathVariable("idTelefone") Long idTelefone) {
-
-		Pessoa pessoa = telefoneRepository.findById(idTelefone).get().getPessoa();
-
-		telefoneRepository.deleteById(idTelefone);
-
-		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
-		modelAndView.addObject("pessoaObj", pessoa);
-		modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoa.getId()));
-		return modelAndView;
-	}
 }
 
